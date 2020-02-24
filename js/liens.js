db9e91aa8d9709bf9383c0223f5e61b5d82eb5e7
flnh <flnh57@gmail.com>
2020-02-24 01:39:45 +0100

    Link now working, change order of projects (most recent to older)

--- js/liens.js
+++ js/liens.js
@@ -3,7 +3,7 @@ var iframes = document.querySelectorAll('iframe');
 window.addEventListener('load', function () {
   iframes.forEach(function (iframe) {
     iframe.contentWindow.document.addEventListener('click', function (e) {
-      e.preventDefault;
+      window.open(e.toElement.baseURI, '_blank');
       console.log(e.toElement.baseURI);
     })
   })
