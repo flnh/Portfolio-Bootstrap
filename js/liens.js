83bf37432e9f56d7a523e65ccf3bea8079264392
flnh <flnh57@gmail.com>
2020-02-26 14:28:57 +0100

    Change color !

--- js/liens.js
+++ js/liens.js
@@ -8,7 +8,7 @@ window.addEventListener('load', function () {
   }, 1050);
 
   iframes.forEach(function (iframe) {
-    iframe.contentWindow.document.querySelector('body').insertAdjacentHTML('afterBegin', '<div style="width:100vw;height:100vh;position:absolute;background-color:rgba(255,255,255,0.8);top:0;left:0;z-index:9999;margin:0;padding:0;"><h2 style="font-size:24px;color:black;text-align:center;margin-block-start:0.83em;">'+iframe.innerHTML+'</h2></div>')
+    iframe.contentWindow.document.querySelector('body').insertAdjacentHTML('afterBegin', '<div style="width:100vw;height:100vh;position:absolute;background-image:linear-gradient(to bottom,rgba(255,255,255,0.9),transparent);top:0;left:0;z-index:9999;margin:0;padding:0;"><h2 style="font-size:24px;color:black;text-align:center;margin-block-start:0.83em;font-family:helvetica,sans-serif;">'+iframe.innerHTML+'</h2></div>')
 
     iframe.contentWindow.document.addEventListener('click', function (e) {
       window.open(e.toElement.baseURI, '_blank');
